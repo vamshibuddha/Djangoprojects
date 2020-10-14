608a44380a074bb64238039c5c12789a1cab2838
vamshibuddha <vamshi.buddha6@gmail.com>
2020-10-14 19:20:33 +0200

    Validation password concept

--- feedbackproject/testApp/forms.py
+++ feedbackproject/testApp/forms.py
@@ -5,15 +5,24 @@ class FeedbackForm(forms.Form):
     name = forms.CharField()
     rollno = forms.IntegerField()
     email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput)
+    rpassword = forms.CharField(label='password(again)', widget=forms.PasswordInput)
     feedback = forms.CharField(widget=forms.Textarea, validators=[validators.MaxLengthValidator(40)
                                , validators.MinLengthValidator(6)])
+    bot_handler = forms.CharField(required=False, widget=forms.HiddenInput)
 
     def clean(self):
         cleaned_data = super().clean()
         inputname = self.cleaned_data['name']
         if len(inputname) < 5:
             raise forms.ValidationError('Name should be minimum 5 charaters')
-
+        inputpassword = self.cleaned_data['password']
+        inputrpassword = self.cleaned_data['rpassword']
+        bot_handler_value = cleaned_data['bot_handler']
+        if len(bot_handler_value) > 0 :
+            raise  forms.ValidationError('Thanks Bot!!')
+        if inputpassword != inputrpassword:
+            raise forms.ValidationError('Passwords not matched')
         '''  def clean_name(self):
         inputname = self.cleaned_data['name']
         if len(inputname) < 4:
