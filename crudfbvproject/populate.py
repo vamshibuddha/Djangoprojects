cd99a05f529b32dc371265b1bf289be20ae69347
vamshibuddha <vamshi.buddha6@gmail.com>
2020-10-21 11:48:33 +0200

    CRUD concept using Employee details with create new employee, delete an employee, update an employee, retrieve data from database

---
+++ crudfbvproject/populate.py
@@ -0,0 +1,23 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crudfbvproject.settings')
+import django
+django.setup()
+
+
+from testapp.models import Employee
+from faker import Faker
+from random import *
+faker = Faker()
+
+
+def populate(n):
+    for i in range(n):
+        feno = randint(1001, 9999)
+        fename = faker.name()
+        fesal = randint(10000, 50000)
+        feaddr = faker.city()
+        emp_record = Employee.objects.get_or_create(eno=feno, ename=fename,esal=fesal,
+                                                    eaddr=feaddr)
+
+
+populate(20)
---
+++ crudfbvproject/testapp/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render,redirect
+from testapp.models import Employee
+from testapp.forms import EmployeeForm
+
+
+# Create your views here.
+def retrieve_view(request):
+    employees = Employee.objects.all()
+    return render(request, 'testapp/index.html', {'employees': employees})
+
+
+def create_view(request):
+    form = EmployeeForm()
+    if request.method == "POST":
+        form = EmployeeForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/index')
+    return render(request, 'testapp/create.html', {'form': form})
+
+
+def update_view(request, id):
+    employee = Employee.objects.get(id=id)
+    if request.method == "POST":
+        form = EmployeeForm(request.POST, instance=employee)
+        if form.is_valid():
+            form.save()
+        return redirect('/index')
+    return render(request, 'testapp/update.html', {'employee': employee})
+
+
+def delete_view(request, id):
+    employee = Employee.objects.get(id=id)
+    employee.delete()
+    return redirect('/index')
