41c561ec7e2ee94251921c37506de6aed33d82db
vamshibuddha <vamshi.buddha6@gmail.com>
2020-10-21 18:31:56 +0200

    Class Based View with ListView, DetailView(R)

---
+++ cbvproject2/testapp/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.views.generic import ListView,DetailView
+from testapp.models import Book
+
+
+# Create your views here.
+class BookListView(ListView):
+    model = Book
+    #template_name = 'testapp/books.html'
+    #context_object_name = 'list_of_books'
+    #default template: book_list.html
+    #default context object : book_list
+
+
+class BookDetailView(DetailView):
+    model = Book
+    #default tempalte name: book_detail.html
+    #default context: book or object
